65d2830e742400b41d06f6f7954ee2f9d01060d5
Arling Holguin <arlingholguin@gmail.com>
2022-10-19 14:25:11 -0500

    first commit

---
+++ src/main/java/com/arling/arlingmarket/ArlingMarketApplication.java
@@ -0,0 +1,13 @@
+package com.arling.arlingmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArlingMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArlingMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arling/arlingmarket/ArlingMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arling.arlingmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArlingMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
